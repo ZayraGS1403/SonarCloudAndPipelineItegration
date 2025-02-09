0f6ce19215ef0c3a43343738de3d07eeccc461c3
ZayraGS1403 <142755654+ZayraGS1403@users.noreply.github.com>
2025-02-09 15:58:49 -0500

    ADD: SECRESTS

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, SonarCloud!");
+        //
     }
 
     public int sum(int a, int b) {
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,10 +3,12 @@ package org.example;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+
 public class MainTest {
     @Test
     public void testSum() {
         Main app = new Main();
         assertEquals(5, app.sum(2, 3));
     }
+
 }
