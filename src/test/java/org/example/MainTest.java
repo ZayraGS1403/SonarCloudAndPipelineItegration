1bf7db0bb8cd1d25789357efd6459f4273dd0a6c
ZayraGS1403 <142755654+ZayraGS1403@users.noreply.github.com>
2025-02-09 14:19:16 -0500

    ADD:First commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testSum() {
+        Main app = new Main();
+        assertEquals(5, app.sum(2, 3));
+    }
+}
